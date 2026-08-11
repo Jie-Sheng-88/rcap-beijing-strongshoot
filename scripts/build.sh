6834359e5c4b900dd2d3045a1469a84ec8bbb63f
Hao Wen <haowentan5@gmail.com>
2026-08-11 16:23:01 +0800

    git ignore things, disable role change

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,9 @@
 cd `dirname $0`
 cd ..
 
+unset VIRTUAL_ENV
+export PATH=$(echo "$PATH" | tr ':' '\n' | grep -v -E 'venv/bin/?$' | paste -sd:)
+
 export PYTHONWARNINGS="ignore::DeprecationWarning,ignore::UserWarning,ignore::FutureWarning"
 
 # colcon build  --symlink-install  --parallel-workers $(nproc) "$@"
