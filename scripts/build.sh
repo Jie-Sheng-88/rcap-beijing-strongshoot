dca1cda12afc6ec99f1f6f1aac98cf379829ffc0
jing0309 <jing030900@gmail.com>
2026-08-14 19:45:29 +0800

    add --symlink-install in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ export PATH=$(echo "$PATH" | tr ':' '\n' | grep -v -E 'venv/bin/?$' | paste -sd:
 
 export PYTHONWARNINGS="ignore::DeprecationWarning,ignore::UserWarning,ignore::FutureWarning"
 
-# colcon build  --symlink-install  --parallel-workers $(nproc) "$@"
-colcon build --parallel-workers $(nproc) "$@"
+colcon build  --symlink-install  --parallel-workers $(nproc) "$@"
+# colcon build --parallel-workers $(nproc) "$@"
 
 espeak "build complete" >/dev/null 2>&1 || echo "Build complete"
